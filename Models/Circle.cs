fe77270419e1331b831166e319a5be0a5f1d8753
knaevKMK <71128514+knaevKMK@users.noreply.github.com>
2023-01-09 18:04:35 +0200

    Add cirlce

---
+++ Models/Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Circle : BaseShape
+    {
+        public decimal R { get; set; }
+
+        public Circle(string r)
+        {
+            R = decimal.Parse(r);
+
+        }
+
+        public decimal D { get => decimal.Multiply(2, R); }
+        public override string Area()
+        {
+            decimal v = decimal.Multiply(new decimal(Math.PI), decimal.Multiply(R,R));
+            return $"{v}";
+        }
+    }
+}
--- Shapes/Program.cs
+++ Shapes/Program.cs
@@ -5,6 +5,7 @@ List<IShape> shapes = new()
     new Triangle("3","4","6"),
     new Rectangle("3","6"),
     new Square("4"),
+    new Circle("4"),
 };
 
 // add new shapes
